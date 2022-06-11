86156121c0b1b79db1750638f242d9abb198ad4a
Robert ANTALUCA <102813576+rantaluca@users.noreply.github.com>
2022-06-11 10:48:15 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,8 +1,8 @@
 <?php
 $dbhost = 'tuxa.sme.utc';
-$dbuser = 'XXXXXXXXX';
-$dbpass = 'XXXXXXXXX';
-$dbname = 'XXXXXXXXX';
+$dbuser = 'XXXXXXXXX'; //username 
+$dbpass = 'XXXXXXXXX'; //password 
+$dbname = 'XXXXXXXXX'; //* 
 
 $connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ('Error connecting to mysql');
 mysqli_set_charset($connect, 'utf8');
